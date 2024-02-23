1c7ef870c09994d2ef7193c2ebb4eae2afe2e52c
Gabriel0598 <augustogabriel35@gmail.com>
2024-02-22 21:32:14 -0300

    Function add_item_columns

---
+++ pyspark_venv/Sample/Functions/add_item_columns.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import size, col
+
+# Create a SparkSession
+spark = (SparkSession
+            .builder
+                .master("local[*]")
+                    .appName("Create Item Columns")
+                        .getOrCreate())
+
+# Function to add columns with array items
+def add_item_columns(df: DataFrame, column: str) -> DataFrame:
+    """
+    Add a new column for each item in the array column.
+
+    :param df: Source DataFrame
+    :param column: Column with the array
+    :return: DataFrame with new columns
+    """
+    array_size = df.select(size(col(column)).alias("array_size")).rdd.max()["array_size"]
+    for i in range(array_size):
+        df = df.withColumn(f"{column}_item_{i}", col(column).getItem(i))
+    return df
+
+# Source dataframe that will be extracted, could be read from table, parque, avro, etc...
+df_source = spark.read.table("db_src.tb_src")
+
+# It's necessary convert source dataframe to df in order to apply in function
+df = df_source
+
+# Apply the function to each column
+columns_to_transform = ['col1', 'col2', 'col3', 'etc']
+for column in columns_to_transform:
+    df = add_item_columns(df, column)
+
+# This new schema should have the new columns
+df.printSchema()
