811325397643e6ed5ac6102eb5062bf02dd3a0ff
Gabriel Augusto Ferreira <gabrielaugustoferreira@outlook.com>
2025-05-12 22:22:35 -0300

    Change master to local

--- pyspark_venv/Sample/read_csv.py
+++ pyspark_venv/Sample/read_csv.py
@@ -7,7 +7,7 @@ from pyspark.sql.functions import *
 from deltalake import DeltaTable
 
 spark = (SparkSession.builder()
-                        .master("local[1]")
+                        .master("local[1]")                                                                                                                                                                                                                                             
                             .config("spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension")
                                 .config("spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog")
                                     .appName("delta pyspark")
